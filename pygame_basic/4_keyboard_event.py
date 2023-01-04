cea903c906d614aa396313310e3076b94188073c
heebum99 <heebum9955@naver.com>
2023-01-04 15:02:17 +0900

    Part6_키보드 이벤트 처리

---
+++ pygame_basic/4_keyboard_event.py
@@ -0,0 +1,82 @@
+import pygame
+
+pygame.init()  # 초기화 (반드시 필요)
+
+# 화면 크기 설정
+screen_width = 480  # 가로 크기
+screen_height = 640  # 세로 크기
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# 화면 타이틀 설정
+pygame.display.set_caption("Nado Game")  # 게임 이름
+
+# 배경 이미지 불러오기
+background = pygame.image.load(
+    "C:\\파이썬\\ap_WorkSpace_1\\Python-practice_application\\pygame_basic\\background.png")
+
+# 캐릭터(스프라이트) 이미지 불러오기
+character = pygame.image.load(
+    "C:\\파이썬\\ap_WorkSpace_1\\Python-practice_application\\pygame_basic\\character.png")
+character_size = character.get_rect().size  # 이미지의 크기를 구함
+character_width = character_size[0]  # 캐릭터의 가로크기
+character_height = character_size[1]  # 캐릭터의 세로크기
+# 화면 가로의 절반 크기에 해당하는 곳에 위치
+character_x_pos = (screen_width / 2) - (character_width/2)
+character_y_pos = screen_height - character_height  # 화면 세로 크기 가장 아래에 해당하는 곳에 위치
+
+# 이동할 좌표
+to_x = 0
+to_y = 0
+
+# 이벤트 루프 => 프로그램이 종료되지 않도록 대기하는것으로 마우스나 키보드 입력을 체크하는것
+# 이벤트 루프가 있어야 화면이 종료되지 않으므로 설정
+running = True  # 게임이 진행중인가?
+while running:
+    for event in pygame.event.get():  # 어떤 이벤트가 발생했는지
+        if event.type == pygame.QUIT:  # 창이 닫히는 이벤트가 발생하면
+            running = False  # 게임이 진행중이 아님
+
+        if event.type == pygame.KEYDOWN:  # 키보드를 눌렀을 경우
+            if event.key == pygame.K_LEFT:  # 캐릭터를 왼쪽으로
+                to_x -= 5
+            elif event.key == pygame.K_RIGHT:  # 캐릭터를 오른쪽으로
+                to_x += 5
+            elif event.key == pygame.K_UP:  # 캐릭터를 위로
+                to_y -= 5
+            elif event.key == pygame.K_DOWN:  # 캐릭터를 아래로
+                to_y += 5
+
+        if event.type == pygame.KEYUP:  # 방향키를 땠을 경우
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:  # x방향으로의 이동을 멈춤
+                to_x = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:  # y방향으로의 이동을 멈춤
+                to_y = 0
+
+    # 이동한 만큼 캐릭터의 좌표를 수정
+    character_x_pos += to_x
+    character_y_pos += to_y
+
+    # 캐릭터의 가로 경계값 처리(화면 밖으로 벗어나지 못하게)
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+
+    # 캐릭터의 세로 경계값 처리
+    if character_y_pos < 0:
+        character_y_pos = 0
+    elif character_y_pos > screen_height - character_height:
+        character_y_pos = screen_height - character_height
+
+    # 배경그리기, blit()로 background 이미지를 시작좌표에 맞게 그린다.
+    screen.blit(background, (0, 0))
+
+    # 캐릭터 그리기, blit()로 character 이미지를 시작좌표에 맞게 그린다.
+    screen.blit(character, (character_x_pos, character_y_pos))
+
+    # pygame에서 매 프레임마다 화면을 새로 그리는 동작을 해줘야함
+    pygame.display.update()  # 게임화면을 다시 그리기!
+
+
+# pygame 종료
+pygame.quit()
